37fac5c4d5e8525f395a43e05d5da5395c4970c3
reshayadav <70374213+reshayadav@users.noreply.github.com>
2021-08-06 22:37:03 +0530

    Update In.js

--- screens/In.js
+++ screens/In.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text } from "react-native";https://github.com/reshayadav/Project68/blob/master/screens/In.js
 
 export default class In extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", AlignItems: "center" }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text style={(color = "#A93AA7")}>Instagram </Text>
       </View>
     );
