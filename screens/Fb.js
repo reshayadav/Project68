3b404c3c34f7842c37d288aa166d1927ef8e9772
reshayadav <70374213+reshayadav@users.noreply.github.com>
2021-08-06 22:36:32 +0530

    Update Fb.js

--- screens/Fb.js
+++ screens/Fb.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default class Fb extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", AlignItems: "center" }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Text style={color = "#4867AA"}>Facebook </Text>
       </View>
     );
